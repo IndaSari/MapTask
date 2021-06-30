a42fd3578bc51b6cfcda01e32c1a45997e5c5682
IndaSari <indasaripuddin@gmail.com>
2021-07-01 01:22:18 +0800

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -12,10 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // user
 app.get("/api/users/:id", getUser);
-
 // messages
 app.post("/api/messages", postMessages);
 app.put("/api/messages/:id", putMessage);
-
 // tslint:disable-next-line:no-console
 app.listen(port, () => console.log(`Listening on port ${port}`));
